ccd9ad140ff879b2ab751847874153860e421488
Shawn Leberknight <shawnleberknight@Shawns-MacBook-Pro.local>
2016-05-15 20:47:43 -0400

    added more stuff

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@
 
 debugger;
 
+var cool = require('cool-ascii-faces');
 var express = require('express');
 var parser = require('body-parser');
 var router = require('./api');
@@ -15,6 +16,10 @@ app.use('/', express.static('public'));
 app.use(parser.json());
 
 app.use('/api', router);
+
+app.get('/cool', function(request, response) {
+  response.send(cool());
+});
 
 app.listen(3000, function() {
     console.log('The server is running on port 3000');
